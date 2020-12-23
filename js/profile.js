88fa3c92925d63a6632cab559dc7a040130c36ae
Akshar <sharad.akshar@gmail.com>
2020-12-23 19:48:10 +0530

    replaced lists with table in profile page

---
+++ js/profile.js
@@ -0,0 +1,16 @@
+var name = 'Akshar';
+var age = 32;
+var home = 'Hyderabad';
+var caste = 'Padmanayaka Velama';
+
+var nameEl = document.getElementById('pname');
+nameEl.textContent = name;
+
+var ageEl = document.getElementById('page');
+ageEl.textContent = age;
+
+var homeEl = document.getElementById('phome');
+homeEl.textContent = home;
+
+var casteEl = document.getElementById('pcaste');
+casteEl.textContent = caste;
